6fc9c3ac0bc2f3bc5927a556052dd481d768009e
Murdoch <programming.tiger.2018@gmail.com>
2025-12-09 21:12:06 -0600

    Forgot to commit part 1

--- Day_5/Part_1/solution.py
+++ Day_5/Part_1/solution.py
@@ -2,14 +2,33 @@ import sys
 
 class Solution:
     def __init__(self) -> None:
-        pass
+        self.ranges = []
+
+    def parseRange(self, line):
+        parts = line.split("-")
+        self.ranges.append([int(parts[0]), int(parts[1])])
 
     def solve(self):
+        sum = 0
         inputFile = open(sys.argv[1], "r")
 
         for line in inputFile:
             sanitizedLine = line.rstrip()
-            print(sanitizedLine)
+
+            # skip any empty lines
+            if len(sanitizedLine) == 0:
+                continue
+
+            if "-" in sanitizedLine:
+                self.parseRange(sanitizedLine)
+            else:
+                val = int(sanitizedLine)
+                for x in self.ranges:
+                    if x[0] <= val and val <= x[1]:
+                        sum += 1
+                        break
+
+        print("Sum is ", sum)
 
 def main():
     solver = Solution()
