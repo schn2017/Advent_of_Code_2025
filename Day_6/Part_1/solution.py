df84f06dc96b9e044510653f62552285655b8b38
Murdoch <programming.tiger.2018@gmail.com>
2025-12-11 20:20:57 -0600

    Day 6 part 1 done

--- Day_6/Part_1/solution.py
+++ Day_6/Part_1/solution.py
@@ -1,15 +1,57 @@
 import sys
 
+class Problem:
+    def __init__(self) -> None:
+        self.numbers = []
+        self.operator = ""
+
+    def addNumber(self, number: int):
+        self.numbers.append(number)
+
+    def setOperator(self, op: str):
+        self.operator = op
+
 class Solution:
     def __init__(self) -> None:
-        pass
+        self.problems: list[Problem] = []
 
     def solve(self):
         inputFile = open(sys.argv[1], "r")
 
-        for line in inputFile:
+        for index, line in enumerate(inputFile):
             sanitizedLine = line.rstrip()
-            print(sanitizedLine)
+            parts = sanitizedLine.split(" ")
+
+            partIdx = 0
+            for part in parts:
+                if len(part) == 0:
+                    continue
+                
+                if index == 0:
+                    newProblem = Problem()
+                    self.problems.append(newProblem)
+                
+                if part.isnumeric():
+                    self.problems[partIdx].addNumber(int(part))
+                else:
+                    self.problems[partIdx].setOperator(part)
+
+                partIdx += 1
+
+        sum = 0
+        for problem in self.problems:
+            if problem.operator == "*":
+                product = 1
+
+                for num in problem.numbers:
+                    product *= num
+
+                sum += product
+            elif problem.operator == "+":   
+                for num in problem.numbers:
+                    sum += num
+         
+        print("The sum is", sum)
 
 def main():
     solver = Solution()
